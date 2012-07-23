eb0f1bc339d3b71b0a006ca7c69d5f3f06ee95b9
Jory Graham <jory@Magellan.local>
2012-07-23 14:04:06 -0400

    A fresher version of init.el

--- init.el
+++ init.el
@@ -1,15 +1,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; init.el
+;; Install all the required packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'package)
-;; (add-to-list 'package-archives
-;; 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -20,13 +19,6 @@
                                   yaml-mode sass-mode
                                   rainbow-delimiters rainbow-mode
                                   fill-column-indicator))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Packages that I might have to install
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; elisp-slime-nav, find-file-in-project, idle-highlight-mode,
-;; ido-ubiquitous, magit, paredit, smex
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -42,158 +34,52 @@
     (load custom-file))
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
-(global-set-key (kbd "<select>") 'windmove-up)
-
-(require 'pbcopy)
-(turn-on-pbcopy)
+(server-start)
 
 (load-theme 'zenburn)
 
-(global-set-key (kbd "\C-xf") 'find-file-in-project)
-
-(add-hook 'dired-mode-hook 'ensure-buffer-name-ends-in-slash)
-(defun ensure-buffer-name-ends-in-slash ()
-  "change buffer name to end with slash"
-  (let ((name (buffer-name)))
-    (if (not (string-match "/$" name))
-        (rename-buffer (concat "/" name "/") t))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Generic customizations
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (defun safe-load (theme)
-;;   (if (find theme (custom-available-themes))
-;;       (load-theme theme t)))
-
-;; (safe-load 'zenburn)
-;; (safe-load 'Consolas)
-(winner-mode t)
-
-(server-start)
-
-(when (eq system-type 'darwin) ;; mac specific settings
-  ;; (setq mac-command-key-is-meta t)
-  ;; (setq mac-command-modifier 'meta)
-
-  ;; OS X's ls command doesn't support the --dired flag, so use the
-  ;; built-in one instead. This is the default behaviour on Windows.
-  ;;     See: dired-use-ls-dired
+(when (eq system-type 'darwin)
+  (require 'pbcopy)
+  (turn-on-pbcopy)
   (setq ls-lisp-use-insert-directory-program nil)
   (require 'ls-lisp))
 
-(require 'pbcopy)
-(turn-on-pbcopy)
 
-;; (add-to-list 'inf-ruby-implementations '("pry" . "pry"))
-;; (setq inf-ruby-default-implementation "pry")
-;; (setq inf-ruby-first-prompt-pattern "^\\[[0-9]+\\] pry\\((.*)\\)> *")
-;; (setq inf-ruby-prompt-pattern "^\\[[0-9]+\\] pry\\((.*)\\)[>*\"'] *")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Modes and Hooks
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; ;; Generic customizations
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(winner-mode t)
+(column-number-mode)
 
-; ;; (defun safe-load (theme)
-; ;;   (if (find theme (custom-available-themes))
-; ;;       (load-theme theme t)))
+;;;;;;;;;;;;;;;;;;;;
+;; Get the mouse working!
+;;;;;;;;;;;;;;;;;;;;
+(require 'mouse)
+(xterm-mouse-mode)
+(defun track-mouse (e))
 
-; ;; (safe-load 'zenburn)
-; ;; (safe-load 'Consolas)
-
-
-; ;; (setq fill-column 80)
-
-
-
-; (defun eshell/clear ()
-;   "04Dec2001 - sailor, to clear the eshell buffer."
-;   (interactive)
-;   (let ((inhibit-read-only t))
-;     (erase-buffer)))
-
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; ;; IRC
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; (require 'bitlbee)
-
-; (setq erc-auth "~/.emacs.d/.erc-auth")
-; (if (file-readable-p erc-auth)
-;     (load erc-auth))
-
-; (defun bitlbee-identify ()
-;   "If we're on the bitlbee server, send the identify command to the &bitlbee channel."
-;   (when (and (string= "localhost" erc-session-server)
-;              (string= "&bitlbee" (buffer-name)))
-;     (erc-message "PRIVMSG" (format "%s identify %s" (erc-default-target)
-;                                    bitlbee-password))))
-
-; (add-hook 'erc-join-hook 'bitlbee-identify)
-
-; (global-set-key (kbd "\C-c bee") (lambda () (interactive)
-;                                    (bitlbee-start)
-;                                    (erc :server "localhost" :port 6667
-;                                         :nick "jory")))
-
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-
-;; (add-to-list 'auto-mode-alist '("\\.hbs$" . html-mode))
-
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; ;; Hooks
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; (defun js-mode-options ()
-;   "These are *my* js-mode options."
-;   (setq js-indent-level 2))
-
-; (add-hook 'js-mode-hook 'js-mode-options)
-
-
-; (defun org-mode-options ()
-;   "These are *my* org-mode options."
-;   (setq fill-column 80)
-;   (longlines-mode -1)
-;   (auto-fill-mode))
-
-; (add-hook 'org-mode-hook 'org-mode-options)
-
-; (defun html-mode-options ()
-;   "These are *my* html-mode options."
-;   (setq fill-column 80)
-;   (setq tab-width 4)
-;   (longlines-mode -1))
-
-; (add-hook 'html-mode-hook 'html-mode-options)
-
-; (remove-hook 'text-mode-hook 'turn-on-auto-fill)
-; (add-hook 'text-mode-hook 'longlines-mode)
-
+;;;;;;;;;;;;;;;;;;;;
+;; Hooks
+;;;;;;;;;;;;;;;;;;;;
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
 (set-default 'tab-width 4)
 (set-default 'indent-tabs-mode nil)
 (setq longlines-auto-wrap t)
-(column-number-mode)
 
-; (set-default 'compilation-scroll-output 'first-error)
+;;;;;;;;;;;;;;;;;;;;
+;; Coffeescript
+;;;;;;;;;;;;;;;;;;;;
+(defun coffee-custom ()
+  (make-local-variable 'tab-width)
+  (set 'tab-width 2))
 
-; ;; (load "~/.emacs.d/vendor/nxhtml/autostart.el")
-; ;; (setq mumamo-chunk-coloring 42)
-; ;; (add-to-list 'auto-mode-alist '("\\.html$" . html-mumamo-mode))
+(add-hook 'coffee-mode-hook 'coffee-custom)
 
-; ;; HACK: Needed to suppress all the Compile-log junk. Should be removed.
-; (setq warning-minimum-level :error)
-
-(add-to-list 'auto-mode-alist '("\\Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\Gemfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\thor$" . ruby-mode))
-
-
-; ;; TODO: Highlighting the TODO keyword in CSS mode.
+;;;;;;;;;;;;;;;;;;;;
+;; CSS / SASS
+;;;;;;;;;;;;;;;;;;;;
 (defun css-options ()
   "My CSS options"
   (setq css-indent-offset 2))
@@ -201,69 +87,38 @@
 (add-hook 'css-mode-hook 'css-options)
 (add-hook 'css-mode-hook 'rainbow-mode)
 
-;; TODO: Add functionality to paredit mode for CSS. Auto {}.
-;; (add-hook 'css-mode-hook 'paredit-mode)
-
 (add-hook 'sass-mode-hook 'rainbow-mode)
 (add-hook 'sass-mode-hook 'rainbow-delimiters-mode)
 
+;;;;;;;;;;;;;;;;;;;;
+;; Dired
+;;;;;;;;;;;;;;;;;;;;
+(put 'dired-find-alternate-file 'disabled nil)
+
+(add-hook 'dired-mode-hook 'ensure-buffer-name-ends-in-slash)
+(defun ensure-buffer-name-ends-in-slash ()
+  "change buffer name to end with slash"
+  (let ((name (buffer-name)))
+    (if (not (string-match "/$" name))
+        (rename-buffer (concat name "/") t))))
+
+;;;;;;;;;;;;;;;;;;;;
+;; HAML
+;;;;;;;;;;;;;;;;;;;;
 (add-hook 'haml-mode-hook 'rainbow-delimiters-mode)
 
-; (add-hook 'js-mode-hook 'flymake-mode)
-; (remove-hook 'js-mode-hook 'auto-fill-mode)
+;;;;;;;;;;;;;;;;;;;;
+;; Magit
+;;;;;;;;;;;;;;;;;;;;
+(setq magit-default-tracking-name-function 'magit-default-tracking-name-branch-only)
 
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; ;; Defuns
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; (defun delete-most-horizontal-space ()
-;   "Usually you want to leave at least one space"
-;   (interactive)
-;   (delete-horizontal-space)
-;   (insert " "))
-
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; ;; Global-set-keys
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; (global-set-key (kbd "\C-c cap") 'org-capture)
-; (global-set-key (kbd "M-\\") 'delete-most-horizontal-space)
-
-; (defun python-server ()
-;   "Run a python http server in the current directory."
-;   (interactive
-;    (async-shell-command "python -m http.server")))
-
-; (global-set-key (kbd "C-c p") 'python-server)
-
-;; TODO: Figure out if this loads if I put it before the flymake
-;; stuff, since it doesn't seem to be loading if it goes afterwards.
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; ;; FLYMAKE
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; (setq flymake-log-level 3)
-
-; (global-set-key (kbd "C-c C-e") 'flymake-display-err-menu-for-current-line)
-; (global-set-key (kbd "C-c C-n") 'flymake-goto-next-error)
-; (global-set-key (kbd "C-c C-p") 'flymake-goto-prev-error)
-
-; (require 'flymake-jshint)
-; (setq jshint-configuration-path nil)
-
-; (require 'flymake-ruby)
-; (add-hook 'ruby-mode-hook 'flymake-ruby-load)
-
-; (require 'flymake-sass)
-; (add-hook 'sass-mode-hook 'flymake-sass-load)
-
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; ;; An idea from https://gist.github.com/1688384
-; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+;;;;;;;;;;;;;;;;;;;;
+;; Ruby
+;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'auto-mode-alist '("\\Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\thor$" . ruby-mode))
 
 (defun ruby-generate-tags()
   (interactive)
@@ -273,39 +128,32 @@
       (if (file-exists-p my-tags-file)
           (delete-file my-tags-file))
       (shell-command
-       (format "find %s -iname '*.rb' -not -regex \".*vendor.*\"| grep -v db | xargs /usr/local/bin/ctags.old -e -a %s"
-               root my-tags-file))
+       (format
+        "find %s -iname '*.rb' -not -regex \".*vendor.*\"| grep -v db | xargs /usr/local/bin/ctags.old -e -a %s"
+        root my-tags-file))
       (if (get-file-buffer my-tags-file)
           (kill-buffer (get-file-buffer my-tags-file)))
       (visit-tags-table my-tags-file))))
 
-; (mouse-wheel-mode 0)
+;;;;;;;;;;;;;;;;;;;;
+;; YAML
+;;;;;;;;;;;;;;;;;;;;
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
-; (setq uniquify-min-dir-content 1)
-
-; (dolist (command '(yank yank-pop))
-;   (eval `(defadvice ,command (after indent-region activate)
-;            (and (not current-prefix-arg)
-;                 (member major-mode '(emacs-lisp-mode lisp-mode
-;                                                      clojure-mode    scheme-mode
-;                                                      haskell-mode    ruby-mode
-;                                                      rspec-mode      python-mode
-;                                                      c-mode          c++-mode
-;                                                      objc-mode       latex-mode
-;                                                      plain-tex-mode))
-;                 (let ((mark-even-if-inactive transient-mark-mode))
-;                   (indent-region (region-beginning) (region-end) nil))))))
-
-(xterm-mouse-mode)
-
-;; TODO: Figure out why it doesn't get here on normal start-up.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Added by Emacs automatically
+;; Keys
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; ;; TODO: Add something for recent files.
 
+(global-set-key (kbd "\C-xg") 'magit-status)
 
+(global-set-key (kbd "\C-xm") 'smex)
+(global-set-key (kbd "\C-x\C-m") 'smex)
 
+(global-set-key (kbd "\C-x\C-b") 'ibuffer)
 
-; (put 'ido-exit-minibuffer 'disabled nil)
-; (put 'narrow-to-region 'disabled nil)
+(global-set-key (kbd "\C-xf") 'find-file-in-project)
+
+;; Needed for iTerm, since Shift-Up sends <select>
+(global-set-key (kbd "<select>") 'windmove-up)
+
